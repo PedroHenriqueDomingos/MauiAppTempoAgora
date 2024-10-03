60126d23c3306a419c996e39817380e398004a85
0200832211040 <0200832211040@LAB03-06>
2024-10-02 22:06:16 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MauiAppTempoAgora.Models;
+
+namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        CancellationTokenSource _cancellationTokenSource;
+        bool _isChekingLocation;
+
+        string? cidade;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                _cancellationTokenSource = new CancellationTokenSource();
+                GeolocationRequest request = new GeolocationRequest (GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                Location? location = await Geolocation.Default.GetLocationAsync(request, _cancellationTokenSource.Token);
+
+                if (location != null)
+                {
+                    lbl_latitude.Text = location.Latitude.ToString();
+                    lbl_longitude.Text = location.Longitude.ToString();
+                }
+
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
